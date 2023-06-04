eb0b93130f1fae72e5a202180d4212bec42dbe82
RafaellEstevam <rafa-estevam@hotmail.com>
2023-06-04 14:02:49 -0300

    Modifying the footer

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -3,22 +3,8 @@ import './Rodape.css'
 const Rodape = () => {
     return(
         <footer className='rodape-organo'>
-           <div className='midias-sociais'>
-                <div>
-                    <img src='./images/fb.png' alt='logo fb'/>
-                </div>
-                <div>
-                    <img src='./images/tw.png' alt='logo tw'/>
-                </div>
-                <div>
-                    <img src='./images/ig.png' alt='logo ig'/>
-                </div>
-           </div>
            <div className='logo-organo'>
                 <img src='./images/logo.png' alt='logo organo'/>
-           </div>
-           <div className='criado-por'>
-                <h4>Desenvolvido por Alura</h4>
            </div>
         </footer>
     )
